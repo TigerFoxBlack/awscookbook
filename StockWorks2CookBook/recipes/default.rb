a90c072be76eb151afbfecf7aa264985872210f2
TigerFoxBlack <t.hibbins@gmail.com>
2017-04-18 09:44:40 +0200

    Update default.rb

--- StockWorks2CookBook/recipes/default.rb
+++ StockWorks2CookBook/recipes/default.rb
@@ -8,3 +8,15 @@
 #
 include_recipe 'git'
 include_recipe 'nginx'
+
+application '/srv/myapp' do
+  git 'https://github.com/example/myapp.git'
+  virtualenv
+  pip_requirements
+  django do
+    migrate true
+  end
+  gunicorn do
+    port 8000
+  end
+end
