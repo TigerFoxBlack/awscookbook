00a4d3448b58ec5aa1afce9fb809ad4794b4314b
TigerFoxBlack <t.hibbins@gmail.com>
2017-04-18 12:57:02 +0200

    Update metadata.rb

--- StockWorks2CookBook/metadata.rb
+++ StockWorks2CookBook/metadata.rb
@@ -1,10 +1,11 @@
 name             'StockWorks2CookBook'
 maintainer       'YOUR_COMPANY_NAME'
 maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+license          'Apache 2.0'
 description      'Installs/Configures StockWorks2CookBook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'application_python', '~> 4.0.0'
-depends 'build-essential', '~> 8.0.1'
+depends 'application_python'
+depends 'build-essential'
+depends 'poise-python'
